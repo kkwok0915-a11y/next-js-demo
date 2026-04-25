8ec41ab306bf0145486523abab147f7744428044
Kwok Chung Kwan <53043168+kwandontcook@users.noreply.github.com>
2026-04-25 16:19:05 +0930

    Fix API issue

--- utils/dataFetch.js
+++ utils/dataFetch.js
@@ -4,7 +4,7 @@ import apiClient from "@/utils/apiClient";
 export async function getData() {
   try {
     // No need to manually check res.ok; Axios throws if it's not
-    const response = await apiClient.get("/v1/api-data1");
+    const response = await apiClient.get("/v1/api-data");
     return response.data;
   } catch (error) {
     console.error("Axios Fetch Error:", error.response?.data || error.message);
