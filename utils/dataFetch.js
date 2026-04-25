995436e72c9fbdf7b43d25aed2f301e071c53905
Kwok Chung Kwan <53043168+kwandontcook@users.noreply.github.com>
2026-04-25 16:06:48 +0930

    Update getData

--- utils/dataFetch.js
+++ utils/dataFetch.js
@@ -2,6 +2,7 @@ import apiClient from "@/utils/apiClient";
 
 export async function getData() {
   const res = await apiClient.get("/v1/api-data");
+  console.log(res);
   if (!res.ok) throw new Error("Failed to fetch data");
   return res.data;
 }
