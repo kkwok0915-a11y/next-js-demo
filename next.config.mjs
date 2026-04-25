91c015d01947c23b61685420d8fa78f17808bc6e
Kwok Chung Kwan <53043168+kwandontcook@users.noreply.github.com>
2026-04-25 13:24:23 +0930

    Update cors config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes under /api
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://express-js-on-vercel-five-tau.vercel.app/",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
