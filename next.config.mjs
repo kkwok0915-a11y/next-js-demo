c1166af0095a2da53cc2b85ecb2720f53972d3ef
Kwok Chung Kwan <53043168+kwandontcook@users.noreply.github.com>
2026-04-25 13:51:49 +0930

    Update config js

--- next.config.mjs
+++ next.config.mjs
@@ -2,27 +2,13 @@
 const nextConfig = {
   reactCompiler: true,
 
-  async headers() {
+  async rewrites() {
     return [
       {
-        // Apply these headers to all routes under /v1
+        // When you call /v1/api-endpoint in Axios...
         source: "/v1/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://express-js-on-vercel-five-tau.vercel.app",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT,OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
+        // ...it actually fetches from your Express app
+        destination: "https://vercel.app*",
       },
     ];
   },
